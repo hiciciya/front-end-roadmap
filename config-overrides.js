72cb9ddc0144b72ab363635adee9c868775dc761
ciciliu <ciciliu@lagou.com>
2021-06-01 20:20:18 +0800

    add CorsEnv alter port

--- config-overrides.js
+++ config-overrides.js
@@ -4,18 +4,35 @@
  * @format
  */
 
-const { override, addWebpackModuleRule } = require("customize-cra");
-
-module.exports = override(
-  addWebpackModuleRule({
-    test: /\.md?$/,
-    use: [
-      {
-        loader: "html-loader",
-      },
-      {
-        loader: "markdown-loader",
-      },
-    ],
-  })
-);
+const { override, addWebpackModuleRule,overrideDevServer } = require("customize-cra");
+module.exports = {
+  webpack:override(
+    addWebpackModuleRule({
+      test: /\.md?$/,
+      use: [
+        {
+          loader: "html-loader",
+        },
+        {
+          loader: "markdown-loader",
+        },
+      ],
+    })
+  ),
+  devServer: overrideDevServer(
+  //  {port:8088}
+  )
+}
+// module.exports = override(
+//   addWebpackModuleRule({
+//     test: /\.md?$/,
+//     use: [
+//       {
+//         loader: "html-loader",
+//       },
+//       {
+//         loader: "markdown-loader",
+//       },
+//     ],
+//   })
+// );
